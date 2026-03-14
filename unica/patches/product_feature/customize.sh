4ec72845024fb8776e551c8e4bb7e30d7f0b30ad
Android-Artisan <romartisan2025@gmail.com>
2026-03-14 17:27:47 +0400

    [TEMP] unica: patches: product_feature: fully dissable res patches to fix screen rec

--- unica/patches/product_feature/customize.sh
+++ unica/patches/product_feature/customize.sh
@@ -119,22 +119,22 @@ if [[ "$(GET_FP_SENSOR_TYPE "$TARGET_FP_SENSOR_CONFIG")" == "optical" ]]; then
     done
 fi
 
-if ! $SOURCE_HAS_QHD_DISPLAY; then
-    if $TARGET_HAS_QHD_DISPLAY; then
-        LOG_STEP_IN "- Applying multi resolution patches"
-
-        DECODE_APK "system" "system/framework/framework.jar"
-        DECODE_APK "system" "system/framework/gamemanager.jar"
-        DECODE_APK "system" "system/priv-app/SecSettings/SecSettings.apk"
-
+# if ! $SOURCE_HAS_QHD_DISPLAY; then
+#     if $TARGET_HAS_QHD_DISPLAY; then
+#         LOG_STEP_IN "- Applying multi resolution patches"
+# 
+#         DECODE_APK "system" "system/framework/framework.jar"
+#         DECODE_APK "system" "system/framework/gamemanager.jar"
+#         DECODE_APK "system" "system/priv-app/SecSettings/SecSettings.apk"
+# 
 #         ADD_TO_WORK_DIR "$MODPATH/resolution/system" "system" "."
 #         ADD_TO_WORK_DIR "e2sxxx" "system" "media"
-        APPLY_PATCH "system" "system/framework/framework.jar" "$SRC_DIR/unica/patches/product_feature/resolution/framework.jar/0001-Enable-FW_DYNAMIC_RESOLUTION_CONTROL.patch"
-        APPLY_PATCH "system" "system/framework/gamemanager.jar" "$SRC_DIR/unica/patches/product_feature/resolution/gamemanager.jar/0001-Enable-dynamic-resolution-control.patch"
-        SET_FLOATING_FEATURE_CONFIG "SEC_FLOATING_FEATURE_COMMON_CONFIG_DYN_RESOLUTION_CONTROL" "WQHD,FHD,HD"
-        LOG_STEP_OUT
-    fi
-fi
+#         APPLY_PATCH "system" "system/framework/framework.jar" "$SRC_DIR/unica/patches/product_feature/resolution/framework.jar/0001-Enable-FW_DYNAMIC_RESOLUTION_CONTROL.patch"
+#         APPLY_PATCH "system" "system/framework/gamemanager.jar" "$SRC_DIR/unica/patches/product_feature/resolution/gamemanager.jar/0001-Enable-dynamic-resolution-control.patch"
+#         SET_FLOATING_FEATURE_CONFIG "SEC_FLOATING_FEATURE_COMMON_CONFIG_DYN_RESOLUTION_CONTROL" "WQHD,FHD,HD"
+#         LOG_STEP_OUT
+#     fi
+# fi
 
 if ! $SOURCE_HAS_HW_MDNIE; then
     if $TARGET_HAS_HW_MDNIE; then
