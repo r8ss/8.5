725fff34a91156c0ccbf0b7f349a1f4980852a20
Android-Artisan <romartisan2025@gmail.com>
2026-03-21 07:52:49 +0100

    unica: patches: telecom: fixup a bomb

--- unica/patches/telecom/customize.sh
+++ unica/patches/telecom/customize.sh
@@ -1,4 +1,4 @@
 LOG_STEP_IN "- Fixing Reboot on call hang up"
 DECODE_APK "system" "system/priv-app/Telecom/Telecom.apk"
-APPLY_PATCH "system" "system/priv-app/Telecom/Telecom.apk" "$SRC_DIR/unica/patches/telecom/system/priv-app/Telecom/Telecom.apk/0001-Modify-SamsungBluetoothRouteManager.patch"
+APPLY_PATCH "system" "system/priv-app/Telecom/Telecom.apk" "$SRC_DIR/unica/patches/telecom/0001-Modify-SamsungBluetoothRouteManager.patch"
 LOG_STEP_OUT
