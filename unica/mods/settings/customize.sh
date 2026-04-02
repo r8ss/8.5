af1749bb7c616c66e759e1af0607b8909bc9d26b
salvogiangri <giangrecosalvo9@gmail.com>
2026-04-02 17:18:14 +0200

    unica: mods: settings: Fixup and enable Unlimited Google Photos MOD
    co-authored-by: Android Artisan <romartisan2025@gmail.com>

--- unica/mods/settings/customize.sh
+++ unica/mods/settings/customize.sh
@@ -3,6 +3,21 @@ DECODE_APK "system" "system/priv-app/SecSettings/SecSettings.apk"
 cp -fa "$SRC_DIR/unica/mods/settings/SecSettings.apk/"* "$APKTOOL_DIR/system/priv-app/SecSettings/SecSettings.apk"
 LOG_STEP_OUT
 
+LOG_STEP_IN "- Enabling Unlimited Google Photos Mod"
+SMALI_PATCH "system" "system/framework/framework.jar" \
+    "smali/android/app/Instrumentation.smali" "replace" \
+    'newApplication(Ljava/lang/Class;Landroid/content/Context;)Landroid/app/Application;' \
+    'invoke-virtual {p0, p1}, Landroid/app/Application;->attach(Landroid/content/Context;)V' \
+    '    invoke-virtual {p0, p1}, Landroid/app/Application;->attach(Landroid/content/Context;)V\n\n    invoke-static {p1}, Lio/mesalabs/unica/SamsungPropsHooks;->init(Landroid/content/Context;)V' \
+    > /dev/null
+SMALI_PATCH "system" "system/framework/framework.jar" \
+    "smali/android/app/Instrumentation.smali" "replace" \
+    'newApplication(Ljava/lang/ClassLoader;Ljava/lang/String;Landroid/content/Context;)Landroid/app/Application;' \
+    'invoke-virtual {p0, p3}, Landroid/app/Application;->attach(Landroid/content/Context;)V' \
+    '    invoke-virtual {p0, p3}, Landroid/app/Application;->attach(Landroid/content/Context;)V\n\n    invoke-static {p3}, Lio/mesalabs/unica/SamsungPropsHooks;->init(Landroid/content/Context;)V' \
+    > /dev/null
+LOG_STEP_OUT
+
 LOG_STEP_IN "- Enabling BSOH in SecSettings"
 
 DECODE_APK "system" "system/priv-app/SecSettings/SecSettings.apk"
