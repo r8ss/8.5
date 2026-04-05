d6b9f026f78fab53fa68cde2a543bf242c5e413e
Android-Artisan <romartisan2025@gmail.com>
2026-04-05 06:47:28 +0200

    unica: mods: updater: fixup

--- unica/mods/updater/customize.sh
+++ unica/mods/updater/customize.sh
@@ -1,3 +1,10 @@
+SKIPUNZIP=1
+
+if ! $ROM_IS_OFFICIAL; then
+    LOG "Build is not official. Skipping"
+    return 0
+fi
+
 ADD_TO_WORK_DIR "$MODPATH" "system" "."
 
 DECODE_APK "system" "system/priv-app/SecSettings/SecSettings.apk"
